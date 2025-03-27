20b4866331a2752476c9ef9e6649f34d4aa9ae34
AndrewI26 <andrewi44@icloud.com>
2025-03-27 18:30:08 -0400

    first commit

---
+++ contact.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Me</title>
+    <link rel="stylesheet" href="styles/globals.css">
+    <link rel="stylesheet" href="styles/contact.css">
+    <link rel="preconnect" href="https://rsms.me/">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+</head>
+
+<body>
+    <nav class="nav">
+        <ul class="nav-list">
+            <li class="nav-item"><a href="index.php">Home</a></li>
+            <li class="nav-item"><a href="gallery.php">Gallery</a></li>
+            <li class="nav-item"><a href="contact.php">Contact</a></li>
+            <li class="nav-item"><a href="admin.php">Admin</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- 
+Created by Andrew Iammancini
+March 27
+Main home page for the website 
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hansans Portfolio</title>
+    <link rel="stylesheet" href="styles/globals.css">
+    <link rel="stylesheet" href="styles/home.css">
+    <link rel="preconnect" href="https://rsms.me/">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+</head>
+
+<body>
+    <nav class="nav">
+        <ul class="nav-list">
+            <li class="nav-item"><a href="index.php">Home</a></li>
+            <li class="nav-item"><a href="gallery.php">Gallery</a></li>
+            <li class="nav-item"><a href="contact.php">Contact</a></li>
+            <li class="nav-item"><a href="admin.php">Admin</a></li>
+        </ul>
+    </nav>
+    <main>
+        <div class="hero">
+            <img class="hero-img" src="images/backdrop.svg" />
+            <h1 class="hero-title">You dream it. <br> I'll <span class="hero-grad-text">design</span> it!</h1>
+        </div>
+    </main>
+</body>
+
+</html>
