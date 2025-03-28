c1c9be9bf3f38a429a8098f27cc63500e6a241fe
AndrewI26 <andrewi44@icloud.com>
2025-03-28 19:49:25 -0400

    added to home page

--- index.php
+++ index.php
@@ -29,6 +29,35 @@ Main home page for the website
         <div class="hero">
             <img class="hero-img" src="images/backdrop.svg" />
             <h1 class="hero-title">You dream it. <br> I'll <span class="hero-grad-text">design</span> it!</h1>
+            <p class="hero-subtitle">I specialize in designing Roblox animations and graphics. Let's get in touch so I can make your ideas come to life.</p>
+            <button class="contact-me">Contact me</button>
+        </div>
+        <div class="about-me">
+            <p class="about-me-subtitle">ABOUT ME</p>
+            <p class="about-me-title">Hi, I'm Hansan</p>
+            <p class="about-me-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+        <div class="quote-container">
+            <p class="quote">“Hasan made me an amazing animation in no time. A pleasure to work with!”</p>
+            <img src="images/4stars.png" class="quote-stars" />
+            <p class="quote-speaker">- Andrew</p>
+        </div>
+        <div class="skills">
+            <div class="skill">
+                <img src="images/dollar.svg" class="skills-img" />
+                <p class="skills-title">Affordable</p>
+                <p class="skills-desc">I work with the customer to come up with a reasonable price specfic to the commision.</p>
+            </div>
+            <div class="skill">
+                <img src="images/eye.svg" class="skills-img" />
+                <p class="skills-title">Eyecatching</p>
+                <p class="skills-desc">My designs are eyecatching and sure to wow your friends in Roblox!</p>
+            </div>
+            <div class="skill">
+                <img src="images/sand-clock.svg" class="skills-img" />
+                <p class="skills-title">Quick</p>
+                <p class="skills-desc">The average project is finished in under two weeks. I am fast!</p>
+            </div>
         </div>
     </main>
 </body>
