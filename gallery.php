dc6a1a7f425bba0da69f950e3ab10b216dc9c1c1
Edwin <edwinwarriner21@gmail.com>
2025-04-02 23:06:39 -0400

    Gallery 3D model test.

--- gallery.php
+++ gallery.php
@@ -9,6 +9,9 @@
     <link rel="stylesheet" href="styles/gallery.css">
     <link rel="preconnect" href="https://rsms.me/">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    
+    <script type="module" src="scripts/index-CimHIg6K.js"></script>
+
 </head>
 
 <body>
@@ -20,6 +23,61 @@
             <li class="nav-item"><a href="admin.php">Admin</a></li>
         </ul>
     </nav>
+
+
+    <div id="header">
+        <h1>Gallery</h1>
+    </div>
+    <div id="headerRender">
+        <canvas class="view" id="mainRender" data-fbx="Hip Hop Dancing.fbx"></canvas>
+        <div id="mainHoverText">
+            <h3>Awesome Dance</h3>
+            <p>This is an awesome dance. Filler text goes here Filler text goes here Filler text goes here Filler text goes here Filler text goes here </p>
+        </div>
+    </div>
+    
+    
+    <div id="renderGrid">
+        <div class="tile">
+            <canvas class="view tileRender tileRenderHover"  data-fbx="Double Dagger Stab.fbx"></canvas>
+            <div class="tileHoverText">
+                    <h3>Double dagger</h3>
+                    <p>Awesome double dagger</p>
+            </div>
+        </div>
+        
+        <div class="tile">
+            <canvas class="view tileRender" data-fbx="Praying.fbx"></canvas>
+            <div class="tileWords">
+                <h3>thing</h3>
+                <p>description</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="thumbnailGrid">
+
+        <div class="tile">
+                <img src="images\gallery\thumbnail1.png">
+        </div>
+        <div class="tile">
+            <img src="images\gallery\thumbnail2.png">
+        </div>
+        <div class="tile">
+                <img src="images\gallery\thumbnail3.png">
+        </div>
+        <div class="tile">
+            <img src="images\gallery\thumbnail4.png">
+        </div>
+    </div>
+    </div>
+
+
+
+    
+
+
+
 </body>
 
 </html>
