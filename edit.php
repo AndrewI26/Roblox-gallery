0891071b679559d494d71c65ce371775f329bdb6
AndrewI26 <andrewi44@icloud.com>
2025-04-02 20:07:00 -0400

    added auth

--- edit.php
+++ edit.php
@@ -1,12 +1,26 @@
+<?php
+$user = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+$password = filter_input(INPUT_POST, "password", FILTER_DEFAULT);
+
+
+if (password_verify($password, password_hash("1234", PASSWORD_DEFAULT)) && $user == "1234") {
+    $msg = "Correct authentication!";
+} else {
+    $msg = "Incorrect authentication. Try again";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Animations</title>
     <link rel="stylesheet" href="styles/globals.css">
-    <link rel="stylesheet" href="styles/admin.css">
+    <link rel="stylesheet" href="styles/contact.css">
     <link rel="preconnect" href="https://rsms.me/">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 </head>
@@ -20,14 +34,7 @@
             <li class="nav-item"><a href="admin.php">Admin</a></li>
         </ul>
     </nav>
-    <form class="login" action="edit.php" method="POST">
-        <p class="login-title">Admin Login Page</p>
-        <p>Username</p>
-        <input class="user" name="username" type="text" />
-        <p>Password</p>
-        <input class="pass" name="password" type="password" />
-        <input type="submit" class="login-btn"/>
-    </form>
+    <?php echo $msg ?>
 </body>
 
 </html>
